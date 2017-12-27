d636dae4deab6bbefaf50f03037c801337381853
Ronald Olsthoorn <ronaldolsthoorn1@gmail.com>
2017-12-27 17:16:19 +0100

    pre remove commit dropbox

--- primecounter/primecounter.py
+++ primecounter/primecounter.py
@@ -11,23 +11,7 @@ class PrimeCounter(QtGui.QApplication):
     def __init__(self, sys_argv):
         super(PrimeCounter, self).__init__(sys_argv)
 
-        if getattr(sys, 'frozen', False):
-            # frozen
-            basepath = os.path.dirname(sys.executable)
-
-            # Ignore warnings in frozen builds
-            import warnings
-            warnings.filterwarnings("ignore")
-        else:
-            # unfrozen
-            basepath = os.path.dirname(os.path.realpath(__file__))
-
-        f = os.path.abspath(os.path.join(basepath, "img/logo.ico"))
-        self.setWindowIcon(QtGui.QIcon(f))
-
         self.mainView = MainView()
-
-        self.mainView.setWindowIcon(QtGui.QIcon(f))
 
         if os.name == 'nt':
             # This is needed to display the app icon on the taskbar on Windows
@@ -42,22 +26,6 @@ if __name__ == '__main__':
 
     # Enable ctrl-c closeable
     signal.signal(signal.SIGINT, signal.SIG_DFL)
-
-    if getattr(sys, 'frozen', False):
-        # frozen
-        basepath = os.path.dirname(sys.executable)
-
-        # Ignore warnings in frozen builds
-        import warnings
-        warnings.filterwarnings("ignore")
-    else:
-        # unfrozen
-        basepath = os.path.dirname(os.path.realpath(__file__))
-
-    logging.config.fileConfig('logging.config')
-    logger = logging.getLogger(__name__)
-
-    logger.info('Session started')
 
     app = PrimeCounter(sys.argv)
 
